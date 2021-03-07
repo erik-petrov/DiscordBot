33b88ab4d82c33afd21aa41b1b12eee1e2a2b399
Erik Petrov <koteman123@hotmail.com>
2021-03-07 15:04:26 +0200

    Add project files.

---
+++ DiscordBot/Program.cs
@@ -0,0 +1,44 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DiscordBot.commands;
+using System;
+using System.Threading.Tasks;
+
+namespace DiscordBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
+            MainAsync().GetAwaiter().GetResult();
+        }
+        static async Task MainAsync()
+        {
+            var discord = new DiscordClient(new DiscordConfiguration()
+            {
+                //хард код токен - потом убрать.
+                Token = "ODE4MDY5MzgxNjY5OTc4MTUy.YESsew.JnosFeFaXm99YfPAZM5k4I8gkeI",
+                TokenType = TokenType.Bot
+            });
+
+            /*discord.MessageCreated += async (s, e) =>
+            {
+                if (e.Message.Content.ToLower().StartsWith("пидр"))
+                    await e.Message.RespondAsync("лёха");
+            };*/
+            //код выше отвечает на пидр
+
+            var commands = discord.UseCommandsNext(new CommandsNextConfiguration()
+            { 
+                StringPrefixes = new[] {".."}
+            });
+
+            commands.RegisterCommands<Greet>();
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+
+        }
+    }
+}
---
+++ DiscordBot/commands/Greet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+namespace DiscordBot.commands
+{
+    class Greet : BaseCommandModule
+    {
+        [Command("pidor")]
+        public async Task GreetCommand(CommandContext ctx, string kto_pidor)
+        {
+            await ctx.RespondAsync($"Кто пидор?\n Конечно же {kto_pidor}.");
+        }
+    }
+}
